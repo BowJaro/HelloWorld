3f77405df8f9bd83b391b4f42c79183fadf2cd27
BowJaro <100690848+BowJaro@users.noreply.github.com>
2022-03-01 19:47:15 +0700

    Update test12.cpp

--- test12.cpp
+++ test12.cpp
@@ -1,5 +1,5 @@
 // test12.cpp : This file contains the 'main' function. Program execution begins and ends there.
-// sinn trào
+// sinn trào (đây là Ý đúng không?)
 // Hello It's Me, SurgeousJP =))))))))
 #include <iostream>
 using namespace std;
