afa3f0749949c41af8b64e40aa4f50db9502ca09
BowJaro <21520932@gm.uit.edu.vn>
2022-03-02 16:41:18 +0700

    Them file 11 len server

---
+++ test11.cpp
@@ -0,0 +1,44 @@
+// test11.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+class CHonSo
+{
+    int nguyen, tu, mau;
+    friend istream& operator>> (istream&, CHonSo&);
+    friend ostream& operator<< (ostream&, CHonSo);
+};
+int main()
+{
+    CHonSo a;
+    cin >> a;
+    cout << a;
+}
+istream& operator>> (istream& is, CHonSo& a)
+{
+    is >> a.nguyen;
+    is >> a.tu;
+    is >> a.mau;
+    return is;
+}
+ostream& operator<< (ostream& os, CHonSo a)
+{
+    os << a.nguyen<<"+";
+    //os << "+";
+    os << a.tu;
+    os << "/";
+    os << a.mau;
+    return os;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
